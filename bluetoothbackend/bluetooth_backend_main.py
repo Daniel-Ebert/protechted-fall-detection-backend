229c42932b460367e64c930c81728a496400e674
Daniel Ebert <web.par@t-online.de>
2022-01-17 18:07:57 +0100

    Bluetooth additions

--- bluetoothbackend/bluetooth_backend_main.py
+++ bluetoothbackend/bluetooth_backend_main.py
@@ -1,5 +1,7 @@
 #https://github.com/matti644/btmon
-import bluetooth
+#https://forum.arduino.cc/t/how-to-perform-read-and-write-between-raspberry-pi-4-and-arduino-nano-ble/628029
+#https://www.programcreek.com/python/example/97796/bluepy.btle.Peripheral
+import bluepy.btle as btle
 import time
 import sys
 
@@ -14,43 +16,17 @@ class BluetoothBackend:
             """self.serverStartup()"""
             time.sleep(10)
             self.streamlit_start.addLog(to_append="Test")
+            self.connect("fff")
 
-
-    def serverStartup(self):
-        s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-
-        server_address = '00:1A:7D:DA:71:06'
-        server_port = 1
-        backlog = 1
-        size = 1024
-
-        s.bind((server_address, server_port))
-        s.listen(backlog)
-        try:
-            print("Awaiting connection")
-            client, clientInfo = s.accept()
-            print("Connection established")
-
-            while 1:
-                j = 0
-                msg = ""
-                for i in range(1, 100):
-                    j += 0.1
-                    msg += str(i) + "," + str(j) + ";"
-                    time.sleep(0.01)
-                    if (i % 10 == 0):
-                        print(msg)
-                        msg += "\n"
-                        client.send(msg)
-                        msg = ""
-
-                    if (i == 100):
-                        i = 0
-                        if (j == 100):
-                            j = 0
-
-        except:
-            print("Closing socket" + str(sys.exc_info()[1]))
-            s.close()
-
-
+    def connect(self, mac: str):
+            """Connect to a device."""
+            from bluepy.btle import Peripheral
+            match_result = re.search(r'hci([\d]+)', self.adapter)
+            if match_result is None:
+                raise BluetoothBackendException(
+                    'Invalid pattern "{}" for BLuetooth adpater. '
+                    'Expetected something like "hci0".'.format(self.adapter))
+            iface = int(match_result.group(1))
+            self._peripheral = Peripheral(mac, iface=iface, addrType=self.address_type)
+            print("Success")
+            print(self._peripheral)
